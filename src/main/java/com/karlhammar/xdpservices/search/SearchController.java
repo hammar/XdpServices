61f7781101b318c3cb656b44761776eb0feb01e8
Karl Hammar <karl@karlhammar.com>
2015-02-24 13:41:06 +0100

    Now passing ODP search filter config as body of POST request
    This is not compliant with religious REST principles but it works.

--- src/main/java/com/karlhammar/xdpservices/search/SearchController.java
+++ src/main/java/com/karlhammar/xdpservices/search/SearchController.java
@@ -1,5 +1,6 @@
 package com.karlhammar.xdpservices.search;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class SearchController {
 
     @RequestMapping("/odpSearch")
     public OdpSearchResult[] odpSearch(@RequestParam(value="queryString", required=true) String queryString, 
-    		@RequestParam(value="filterConfiguration", required=false) OdpSearchFilterConfiguration filterConfiguration) {
+    		@RequestBody(required=false) OdpSearchFilterConfiguration filterConfiguration) {
     	// Simply defer execution to CompositeSearch
     	return CompositeSearch.INSTANCE.runSearch(queryString,filterConfiguration);
     }
